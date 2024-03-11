422fe969af5bc09558ad12120acc0af0acd0f947
Anand Mahamuni <m.anand7203@gmail.com>
2024-03-11 23:00:31 +0530

    Done

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const fs = require("node:fs");
 const port = 8080;
 const hostname = "localhost";
 const dir = "src/";
-const page = "about.html";
+let page = "index.html";
 
 const server = http.createServer((req, res) => {
-	console.log(req.url);
-  fs.readFile(dir + page,"utf8", (err, data) => {
+  console.log(req.url);
+  if (req.url === "/contact") {
+    page = "contact.html";
+  } else if (req.url === "/about") {
+    page = "about.html";
+  } else if (req.url === "/") {
+    page = "index.html";
+  } else {
+    page = "404.html";
+  }
+  fs.readFile(dir + page, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
